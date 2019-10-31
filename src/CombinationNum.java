d44f6cbeac25b9094c5dca235d5f46418d8bca60
zhh <2889398723@qq.com>
2019-10-31 18:16:46 +0800

    给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
    candidates 中的数字可以无限制重复被选取。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/combination-sum
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ src/CombinationNum.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName CombinationNum
+ * @Desc TODO
+ * @Auther 28893
+ * @Date 2019/10/31  16:44
+ * @Veresion 1.0
+ */
+public class CombinationNum {
+   static  List<List<Integer>> lists=new ArrayList<>();
+    public static List<List<Integer>> combinationNum(int[] candidates,int target){
+        List<Integer> list=new ArrayList<>();
+        DFS(candidates,0,list,0,target);
+        return lists;
+    }
+
+    private static void DFS(int[] candidates, int index, List<Integer> list, int sum, int target) {
+        if(index>=candidates.length||sum>target){
+            return;
+        }
+        else if(sum==target){
+            List e=new ArrayList();
+            e.addAll(list);
+            lists.add(e);
+            return;
+        }
+        for(int i=index;i<candidates.length;i++){
+            if(candidates[i]>target){
+                continue;
+            }
+            list.add(candidates[i]);
+            DFS(candidates,i,list,sum+candidates[i],target);
+            list.remove(list.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] n={2,3,7,8};
+        List<List<Integer>> list=combinationNum(n,7);
+        System.out.println(list);
+    }
+}
